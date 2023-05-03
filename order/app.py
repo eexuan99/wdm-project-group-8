7c23b2929c7c10852578f7fa3480a59a1a839529
Pierluigi <pierluigi.negro.00@gmail.com>
2023-05-03 18:01:33 +0200

    added order app code, missing checkout

--- order/app.py
+++ order/app.py
@@ -36,33 +36,82 @@ atexit.register(close_db_connection)
 def create_order(user_id):
     sql_statement = """INSERT INTO order_table (user_id, paid, items, total_price) 
                        VALUES (%s, %s, %s::items[], %s) RETURNING order_id;"""
-    item = (1, 1, 0.99)
-    items_list = [item]
-    central_db_cursor.execute(sql_statement, (1, True, items_list, 0.99))
-    order_id_of_new_row = central_db_cursor.fetchone()[0]
-    central_db_conn.commit()
-    return {"order_id": order_id_of_new_row}
+    #item = (1, 1, 0.99)
+    #items_list = [item]
+    try:
+        central_db_cursor.execute(sql_statement, (user_id, False, [], 0.0))#(1, True, items_list, 0.99)
+        order_id_of_new_row = central_db_cursor.fetchone()[0]
+        central_db_conn.commit()
+    except psycopg2.DatabaseError as error:
+        print(error)
+        return {"error": "User id not found"}, 400
+
+    return {"order_id": order_id_of_new_row}, 200
     # pass
 
 
 @app.delete('/remove/<order_id>')
 def remove_order(order_id):
-    pass
-
+    sql_statement = """ DELETE FROM order_table
+                        WHERE order_id=%s; """
+    try:
+        central_db_cursor.execute(sql_statement, (order_id))
+        central_db_conn.commit()
+    except psycopg2.DatabaseError as error:
+        print(error)
+        return {"error": "Order id not found"}, 400
+    return {'success': f"Removed order {order_id}"}
 
 @app.post('/addItem/<order_id>/<item_id>')
 def add_item(order_id, item_id):
-    pass
+    sql_statement = """UPDATE order_table  
+                       SET items = array_append(items, %s),
+                        total_price=total_price + {
+                            SELECT unit_price
+                            FROM stock
+                            WHERE item_id = %s
+                       } WHERE order_id = %s;"""
+    try:
+        central_db_cursor.execute(sql_statement, (item_id, order_id))
+        central_db_conn.commit()
+    except psycopg2.DatabaseError as error:
+        print(error)
+        return {"error": "Order id not found"}, 400
+    return {'success': f"Added item {item_id} to order {order_id}"}
+
 
 
 @app.delete('/removeItem/<order_id>/<item_id>')
 def remove_item(order_id, item_id):
-    pass
+    sql_statement = """UPDATE order_table  
+                       SET items = f_array_remove_elem1(items, %s), total_price=total_price - {
+                            SELECT unit_price
+                            FROM stock
+                            WHERE item_id = %s
+                       }
+                        WHERE order_id = %s;"""
+    try:
+        central_db_cursor.execute(sql_statement, (item_id, item_id, order_id))
+        central_db_conn.commit()
+    except psycopg2.DatabaseError as error:
+        print(error)
+        return {"error": "Order id not found, or item id not in order"}, 400
+    return {'success': f"Removed item {item_id} to order {order_id}"}
 
 
 @app.get('/find/<order_id>')
 def find_order(order_id):
-    pass
+    sql_statement = """SELECT * FROM order_table WHERE order_id = %s;"""
+    central_db_cursor.execute(sql_statement, (order_id))
+    order = central_db_cursor.fetchone()
+    if order:
+        return {"order_id": order[0],
+                "user_id": order[1],
+                "paid": order[2],
+                "items": order[3],
+                "total_price": order[4],
+                }
+    return {"error": "order not found"}
 
 
 @app.post('/checkout/<order_id>')
