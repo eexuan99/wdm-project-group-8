0558598c441a61836331fe0ceedd9e68bd53b667
Pierluigi <pierluigi.negro.00@gmail.com>
2023-05-22 19:18:41 +0200

    added tables to payment and stock

--- payment/payment_db/payment_schema.sql
+++ payment/payment_db/payment_schema.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS payment (
     credit int NOT NULL CHECK (credit >= 0), 
     PRIMARY KEY (user_id)
 );
+
+CREATE TABLE IF NOT EXISTS messages (
+    order_id int,
+    transaction_number int,
+    sign char(3),
+    PRIMARY KEY (order_id, transaction_number, sign)
+);
--- stock/stock_db/stock_schema.sql
+++ stock/stock_db/stock_schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS stock (
     unit_price int NOT NULL CHECK (unit_price >= 0), 
     PRIMARY KEY (item_id)
 );
+
+CREATE TABLE IF NOT EXISTS messages (
+    order_id int,
+    transaction_number int,
+    sign char(3),
+    PRIMARY KEY (order_id, transaction_number, sign)
+);
